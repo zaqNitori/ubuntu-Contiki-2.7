64c05048d2521fcf8b8e7616998da2cac644f466
zaqNitori <zaq155162@yahoo.com>
2021-10-11 01:01:16 -0700

    Add two Msg_Type

--- os/net/routing/rpl-location/rpl-loc.h
+++ os/net/routing/rpl-location/rpl-loc.h
@@ -2,9 +2,18 @@
 #ifndef RPL_LOC_H
 #define RPL_LOC_H
 #include "net/ipv6/uip.h"
+
+//test for mass_spring_model_localization 
+#define P_0      -44.06//-44.06
+#define gama     24.13//24.13
+#define STD      1.62 // cc2538dk
+#define LOG_e10  2.302585092994L // log_e (10)
+
 typedef enum 
 {
-	Location_Info
+	Location_Info,
+	MASS_SPRING_REQUEST_INFO,
+	Location_Info_From_Client
 }rpl_loc_t;
 
 typedef struct
